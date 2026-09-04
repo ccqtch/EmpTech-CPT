bf07c1c91d935c9f0ac21713f062597a3ae7e5d9
ccqtch <jtajones@addu.edu.ph>
2026-09-04 16:02:46 +0800

    commit

---
+++ EmpTech PT Animations.js
@@ -0,0 +1,13 @@
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, { threshold: 0.2 });
+
+cards.forEach(card => observer.observe(card));
